1be47e8f9e28b67bb32c55a2804c773acf021269
José Antonio Molina <jamolina2010@hotmail.com>
2023-07-01 00:14:02 +0200

    Añadida prueba de test para getGifs()

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas en <GifItem />>', () => {
@@ -11,4 +11,23 @@ describe('Pruebas en <GifItem />>', () => {
         const { container } = render(<GifItem title={ title } url={ url } />);
         expect( container ).toMatchSnapshot();
     });
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={ title } url={ url } />);
+        // screen.debug() muestra la renderización del GifItem
+        //screen.debug();
+        //console.log(screen.getByRole('img').src);
+        //console.log(screen.getByRole('img').alt);
+        //expect( screen.getByRole('img').src ).toBe( url );
+        //expect( screen.getByRole('img').alt ).toBe( title );
+
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    });
+
+    test('debe de mostrar el título en el componente', () => { 
+        render(<GifItem title={ title } url={ url } />);
+        expect( screen.getByText( title ) ).toBeTruthy();
+     });
 } );
