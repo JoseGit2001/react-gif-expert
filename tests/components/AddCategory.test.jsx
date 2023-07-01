38dcfa792048d72f659a26886034696cbad61860
José Antonio Molina <jamolina2010@hotmail.com>
2023-07-01 13:59:07 +0200

    Pruebas en el componente <AddCategory />2

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -16,8 +16,11 @@ describe('Pruebas en <AddCategory />', () => {
      });
 
      test('debe de llamar onNewCategory si el input tiene un valor', () => { 
-        
-        render( <AddCategory onNewCategory={ () => {} } />);
+
+        // jest.fn Creates a mock function. Optionally takes a mock implementation.
+        const onNewCategory = jest.fn();
+
+        render( <AddCategory onNewCategory={ onNewCategory } />);
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
@@ -26,5 +29,29 @@ describe('Pruebas en <AddCategory />', () => {
         fireEvent.submit( form );
         //screen.debug();
         expect( input.value ).toBe('');
+
+        // Comprueba que la función onNewCategory ha sido llamada
+        expect( onNewCategory ).toHaveBeenCalled();
+        // Comprueba que la función onNewCategory ha sido llamada solo 1 vez
+        expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        // Comprueba que la función onNewCategory ha sido llamada con el valor de inputValue
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
       });
+
+      test('no debe de llamar el onNewCategory si el input está vacio', () => { 
+        // jest.fn Creates a mock function. Optionally takes a mock implementation.
+        const onNewCategory = jest.fn();
+
+        render( <AddCategory onNewCategory={ onNewCategory } />);
+
+        const form = screen.getByRole('form');
+
+        fireEvent.submit( form );
+        //screen.debug();
+
+        // Comprueba que la función onNewCategory ha sido llamada 0 veces
+        expect( onNewCategory ).toHaveBeenCalledTimes(0);
+        // Comprueba que la función onNewCategory no haya sido llamada
+        expect( onNewCategory ).not.toHaveBeenCalled();
+       });
  });
